efd0859850dc53fb5c023be6d731f29223830b6e
primeDdev <ObiwaleAyomideMoses>
2024-10-03 01:55:21 +0100

    config9

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,5 @@ import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
-  },
-  build: {
-    ssr: 'src/entry-server.tsx',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          // Remove hash from all asset file names
-          return 'assets/[name][extname]';
-        },
-        entryFileNames: '[name].js',
-        chunkFileNames: 'assets/[name].js',
-      },
-    },
-    // Optionally, specify the directory for the final build files
-    outDir: 'dist',
-  },
+  plugins: [react()],
 });
